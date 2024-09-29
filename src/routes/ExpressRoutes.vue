fced05a3595a0a82b3edb22f6a985d8ce1998ed1
Corbin Crutchley <git@crutchcorn.dev>
2024-09-29 05:22:35 -0700

    feat: add Vue SSR from Express... From Vue 😅

--- src/routes/ExpressRoutes.vue
+++ src/routes/ExpressRoutes.vue
@@ -1,14 +1,46 @@
 <script lang="ts" setup>
+import express, { Request, Response } from "express";
+import {join, dirname} from "path";
+import { fileURLToPath } from 'url';
+import { renderToString } from 'vue/server-renderer'
 import { ExpressRoute } from "../components/router";
-import { Request, Response } from "express";
+import { createApp } from '../../public/app.js'
 
-const handleRoot = (req: Request, res: Response) => {
-  res.send(
-      '<html><head><title>Express Inside of Vue</title></head><body><p>Testing!</p></body></html>'
-  );
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const handleIndex = (req: Request, res: Response) => {
+  const app = createApp();
+  renderToString(app).then(html => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Vue in Express in Vue Example</title>
+        <script type="importmap">
+          {
+            "imports": {
+              "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+            }
+          }
+        <\/script>
+        <script type="module" src="/client.js"><\/script>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+      </body>
+    </html>
+    `)
+  }).catch(reason => {
+    res.status(500).send(reason);
+  })
 };
+
+const handleRoot = express.static(
+    join(__dirname, "../public")
+);
 </script>
 
 <template>
-  <ExpressRoute path="/" method="get" :function="handleRoot" />
+  <ExpressRoute path="/" method="get" :function="handleIndex" />
+  <ExpressRoute path="/" method="use" :function="handleRoot" />
 </template>
